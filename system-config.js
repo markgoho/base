e9370a6f9b50dfe7276fb5523aaaae4b6e87c1e7
Zyzle <n1l0c2501@gmail.com>
2016-06-03 01:34:29 +0100

    new gh-pages version

---
+++ system-config.js
@@ -0,0 +1,68 @@
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {
+    '@angular2-material': 'vendor/@angular2-material',
+    'firebase': 'vendor/firebase/lib/firebase-web.js',
+    'angularfire2': 'vendor/angularfire2'
+};
+/** User packages configuration. */
+var packages = {
+    angularfire2: {
+        defaultExtension: 'js',
+        main: 'angularfire2.js'
+    }
+};
+var materialPackages = [
+    'button',
+    'card',
+    'core',
+    'icon',
+    'input',
+    'list',
+    'sidenav',
+    'toolbar'
+];
+materialPackages.forEach(function (pkg) {
+    packages[("@angular2-material/" + pkg)] = { main: pkg + ".js" };
+});
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+    'app/navigation',
+    'app/+fbtest',
+    'app/auth',
+    'app/+home',
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
